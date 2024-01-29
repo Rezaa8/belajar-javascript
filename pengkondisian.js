a630ad3bc5f044422a3394fe8810448c56ceca78
Rezaa8 <rezaaa8880@gmail.com>
2024-01-29 14:15:55 +0700

    kalku

---
+++ pengkondisian.js
@@ -0,0 +1,16 @@
+var x = prompt("Masukan Angka");
+// alert("Selamat Datang "+ nama);
+
+if(x % 2 == 0) {
+    alert (x + "bilangan genap");
+} else if(x % 2 == 1) {
+    alert(x + "bilangan ganjil");
+} else {
+    alert("Its not a number");
+}
+
+if(confirm("Apakah Kamu Manusia?"))  {
+    location = "https://google.com";
+} else{
+    alert("Kamu Bego");
+}

b5f578d4f88b7e42fc7dd69bb761da725f0c3cf7
Rezaa8 <rezaaa8880@gmail.com>
2024-01-30 08:51:18 +0700

    object

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -5,7 +5,15 @@ siswa.alamat = "bandung";
 siswa.namaLengkap = function() {
     return this.namaDepan + " " + this.namaBelakang;
 };
+var siswa2 ={
+    namaDepan : "Reza",
+    namaBelakang : "Habib",
+    namaLengkap: function () {
+        return this.namaDepan + " " + this.namaBelakang;
+    },
+};
 
+alert("Nama : "+ siswa2.namaLengkap());
 alert("Nama : "+ siswa.namaLengkap());
 // function namaLengkap() {
     
